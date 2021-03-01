0f964c2d9bfcdbb5e01d900bb9677cd5994fe7e2
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2021-03-01 18:19:14 +0100

    context for firebase provided

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import {render} from 'react-dom';
 import App from './App';
 import GlobalStyles from './GlobalStyles'
 import {firebase} from './lib/firebase.prod.js'
+import {FirebaseCntx} from './context/firebase'
 
 render( <>
-  <GlobalStyles/>
-  <App />
+  <FirebaseCntx.Provider value={{firebase}}>
+    <GlobalStyles/>
+    <App />
+  </FirebaseCntx.Provider>
 </>,  
 document.getElementById('root'));
 
