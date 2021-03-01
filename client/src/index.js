ff639621ef0060ac63e8782aa61cd2ef136ee314
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2021-03-01 16:52:12 +0100

    Firebase settings complete | seeding in progress

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import GlobalStyles from './GlobalStyles'
+
 
 render( <>
   <GlobalStyles/>
--- client/src/lib/firebase.prod.js
+++ client/src/lib/firebase.prod.js
@@ -1,11 +1,20 @@
 import Firebase from 'firebase/app'
-import FireStore from 'firebase/firestore'
-import Auth from 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/auth'
+import {seedDatabase} from '../seed'
 
 
-
-const config = {}
+const config = {
+  apiKey: "AIzaSyD-LhwQ8RgCZuyF97WWtCn2LZQGK2MqaQo",
+  authDomain: "netflx-d1bb7.firebaseapp.com",
+  projectId: "netflx-d1bb7",
+  storageBucket: "netflx-d1bb7.appspot.com",
+  messagingSenderId: "424277922354",
+  appId: "1:424277922354:web:6b6b7bc850eab9acf7a22b"
+}
 
 const firebase = Firebase.initializeApp(config);
+
+seedDatabase(firebase)
 
 export { firebase }
