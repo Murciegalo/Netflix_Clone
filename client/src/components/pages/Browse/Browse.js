2ea2326f12297daa1435ac75730ba4b8e5004746
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2021-03-05 17:41:01 +0100

    browse compound comp. in progress

--- client/src/components/pages/Browse/Browse.js
+++ client/src/components/pages/Browse/Browse.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import useContent from '../../hooks/use-content'
-
+import selectMap from '../../helpers/selectMap'
+import BrowseCont from '../../browse/BrowseCont'
 export default function Browse() {
   const { series } = useContent('series')
   const { films } = useContent('films')
+  const slides = selectMap({series, films})
   
-  return (
-    <div>
-      
-    </div>
-  )
+  return <BrowseCont slides={slides} />
 }
---
+++ client/src/components/profile/ProfileCont.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProfileCont({user, setProfile}) {
+  return <div>
+      <p>Selecting profile...</p>
+  </div>
+}
