7c238d9e8ccac6890b6f9d86ef6589d3612c16f4
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2021-03-18 16:59:23 +0100

    Player compound comp. in progress

--- client/src/components/player/Player.js
+++ client/src/components/player/Player.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, {useState, useContext, createContext} from 'react'
+import ReactDom from 'react-dom';
 import {
+  Container,
   Btn,
-  Video
+  Overlay,
+  Inner,
+  Close
 } from './styles'
 
-export default function Player() {
-  return (
-    <div>
-      
-    </div>
-  )
+export const PlayerContext = createContext()
+
+export default function Player({children, ...restProps}) {
+  const [showPlayer, setShowPlayer] = useState(false)
+
+  return <PlayerContext value={{showPlayer, setShowPlayer}}>
+    <Container {...restProps}>{children}</Container>
+  </PlayerContext>
 }
 
-Player.Btn = function PlayerBtn({children, ...restProps}) {
-  return <Btn {...restProps}>{children}</Btn>
+Player.Video = function PlayerVideo({src, ...restProps}) {
+  const {showPlayer, setShowPlayer } = useContext(PlayerContext);
+  
+  const rendered = showPlayer ? ReactDom.createPortal(
+    <Overlay onClick={() => setShowPlayer(false)}>
+      <Inner>
+        <video id='netflix-player' controls>
+          <source src={src} type='video/mp4' />
+        </video>
+        <Close />
+      </Inner>
+    </Overlay>
+  ) : null
+  
+  return rendered;
+}
+Player.Btn = function PlayerBtn({...restProps}) {
+  const {showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+  return <Btn onClick={showPlayer => setShowPlayer(!showPlayer)}>
+    Play
+  </Btn>
 }
 
-Player.Video = function PlayerVideo({children, ...restProps}) {
-  return <Video {...restProps}>{children}</Video>
+Player.Overlay = function PlayerOverlay({children, ...restProps}) {
+  return <Overlay {...restProps}>{children}</Overlay>
+}
+
+Player.Inner = function PlayerInner({children, ...restProps}) {
+  return <Inner {...restProps}>{children}</Inner>
+}
+
+Player.Close = function PlayerClose({children, ...restProps}) {
+  return <Close {...restProps}>{children}</Close>
 }
--- client/src/components/player/styles.js
+++ client/src/components/player/styles.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components/macro'
 import {Link as ReactLink} from 'react-router-dom'
 
+
+export const Container = styled.button`
+  
+`;
+
 export const Btn = styled.button`
   
 `;
 
-export const Video = styled.button`
+export const Overlay = styled.button`
+  
+`;
+
+export const Inner = styled.button`
+  
+`;
+
+export const Close = styled.button`
   
 `;
